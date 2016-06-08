970b6bb914266567be44ed5b596c3570cc8160f7
CodeCoderLee <799320019@qq.com>
2016-06-08 13:14:48 +0800

    取消演示页面中,演示设备列表对注册人的限制

--- src/main/java/cn/ac/bcc/controller/business/device/DeviceController.java
+++ src/main/java/cn/ac/bcc/controller/business/device/DeviceController.java
@@ -95,9 +95,11 @@ public class DeviceController extends BaseController<Device> {
     @ResponseBody
     @RequestMapping("searchByPage")
     public ResponseData searchByPage(Device device, Integer pageNum, Integer pageSize) throws Exception {
-        Integer userId = Common.findUserSessionId(getRequest());
-        device.setRegisterAccount(userId);
-        device.setDebugAccount(userId);
+        if (device.getStatus() != null && device.getStatus() == 1) {
+            Integer userId = Common.findUserSessionId(getRequest());
+            device.setRegisterAccount(userId);
+            device.setDebugAccount(userId);
+        }
         PageHelper.startPage(pageNum, pageSize);
         List<Device> list = deviceService.selectDebugDevice(device);
         PageInfo<Device> pageInfo = new PageInfo<Device>(list);
