3b425c054eefa50df7a028cc833707adb6363caa
CodeCoderLee <799320019@qq.com>
2016-07-08 09:19:34 +0800

    频点管理修改

---
+++ src/main/java/cn/ac/bcc/model/business/ProgramDto.java
@@ -0,0 +1,34 @@
+package cn.ac.bcc.model.business;
+
+/**
+ * Created by lifm on 16/7/7.
+ */
+public class ProgramDto {
+    private String pid;
+    private String name;
+    private boolean select;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelect() {
+        return select;
+    }
+
+    public void setSelect(boolean select) {
+        this.select = select;
+    }
+}
